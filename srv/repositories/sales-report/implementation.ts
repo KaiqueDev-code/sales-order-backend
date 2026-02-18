15e699d978c9928b47a274f2147e2ffa6319b604
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-02-18 16:44:05 -0300

    feat: create findByCustomer method in SalesReportRepository and SalesReportRepositoryImpl

--- srv/repositories/sales-report/implementation.ts
+++ srv/repositories/sales-report/implementation.ts
@@ -13,15 +13,34 @@ export class SalesReportRepositoryImpl implements SalesReportRepository {
         substractedDays.setDate(substractedDays.getDate() - days);
         const substractedDaysISOString = substractedDays.toISOString();
 
-        const sql = SELECT.from('sales.SalesOrderHeader')
+        const sql = this.getReportBaseSql().where({createdAt: {between: substractedDaysISOString, and: today}});
+        const SalesReport = await cds.run(sql);
+        if (SalesReport.length === 0) {
+            return null;
+        }
+        return this.mapReportResult(SalesReport);
+    }
+
+    public async findByCustomer(customerId: string): Promise<SalesReportsModel[] | null> {
+        const sql = this.getReportBaseSql().where({customer_id : customerId});
+        const SalesReport = await cds.run(sql);
+        if (SalesReport.length === 0) {
+            return null;
+        }
+        return this.mapReportResult(SalesReport);
+    }
+
+    private getReportBaseSql (): cds.ql.SELECT<unknown, unknown >{
+        return SELECT.from('sales.SalesOrderHeader')
         .columns(
             'id as SalesOrderId',
             'totalAmount as salesOrderTotalAmount',
             'customer.id as customerId',
             'customer.firstName || " " || customer.lastName as customerFullName'
         )
-        .where({createdAt: {between: substractedDaysISOString, and: today}});
-        const SalesReport = await cds.run(sql);
+    }
+
+    private mapReportResult (SalesReport: SalesReportByDays[]): SalesReportsModel[] | null{
         if (SalesReport.length === 0) {
             return null;
         }
--- srv/repositories/sales-report/protocols.ts
+++ srv/repositories/sales-report/protocols.ts
@@ -2,4 +2,5 @@ import { SalesReportsModel } from "@/models/sales-report";
 
 export interface SalesReportRepository{
     findBydays(days: number): Promise<SalesReportsModel[] | null>;
+    findByCustomer(customerId: string): Promise<SalesReportsModel[] | null>;
 } 
