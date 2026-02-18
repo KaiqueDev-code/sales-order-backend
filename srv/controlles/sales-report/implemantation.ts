ca4b238daa75bbc7ee9213490dded33a6ca0f660
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-02-18 15:14:04 -0300

    fix: bug imports

--- srv/controlles/sales-report/implemantation.ts
+++ srv/controlles/sales-report/implemantation.ts
@@ -1,11 +1,11 @@
-import { ExpectedResult } from "@models/db/types/SalesReportByDays";
-import { salesReportController } from "./protocols";
+import { ExpectedResult as SalesReportByDays} from "@models/db/types/SalesReportByDays";
+import { SalesReportController } from "./protocols";
 import { SalesReportService } from "@/services/sales-report/protocols";
 
-export class SalesReportControllerImpl implements salesReportController {
+export class SalesReportControllerImpl implements SalesReportController {
     constructor(private readonly service: SalesReportService){}
 
-    findBydays(days: number): Promise<ExpectedResult[]> {
+    findBydays(days: number): Promise<SalesReportByDays[]> {
         return this.service.findBydays(days);
     }
 }
