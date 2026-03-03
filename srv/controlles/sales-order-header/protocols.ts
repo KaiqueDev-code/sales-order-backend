dd0051b1b9061a68736bf3d407a42f40fd30cec3
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-03 09:20:05 -0300

    refactor: remove unused customer and sales order header controllers and protocols

--- srv/controlles/sales-order-header/protocols.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '@sap/cds';
-
-import { CreationPayloadValidationResult } from '@/services/sales-order-header/protocols';
-import { SalesOrderHeaderModel } from '@/models/sales-order-header';
-
-export interface SalesOrderHeaderController {
-    beforeCreate(params: SalesOrderHeaderModel): Promise<CreationPayloadValidationResult>;
-    afterCreate(params: SalesOrderHeaderModel, loggedUser: User): Promise<void>;
-}
--- srv/controlles/sales-report/protocols.ts
+++
@@ -1,7 +0,0 @@
-import { ExpectedResult as SalesReportByDays} from "@models/db/types/SalesReportByDays";
-
-
-export interface SalesReportController {
-     findBydays(days: number): Promise<SalesReportByDays[]>;
-     findByCustomerId(customerId: string): Promise<SalesReportByDays[]>;
-}
