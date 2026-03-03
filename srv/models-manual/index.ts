8c8b0c7f5202a33b1cd881e2018bccc88b752eca
Kaique Santana de Almeida <kaiquesantanadealmeida580@gmail.com>
2026-03-03 09:41:33 -0300

    feat: add CustomerModel and LoggedUserModel with properties and methods

---
+++ srv/models-manual/index.ts
@@ -0,0 +1,7 @@
+export * from './customers';
+export * from './products';
+export * from './sales-order-header';
+export * from './sales-order-items';
+export * from './sales-order-logs';
+export * from './sales-report';
+export * from './logged-user';
